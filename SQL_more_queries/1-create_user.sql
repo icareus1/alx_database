435df9879c329d45ea2bf563dfaa05f5b9013c9f
icareus1 <wamars@live.fr>
2023-08-15 11:26:58 +0000

    Mysql More queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -5,5 +5,5 @@
 -- If the user user_0d_1 already exists, your script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
